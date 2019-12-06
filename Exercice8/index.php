f0aa82e35b39a35f8b5c89eeab886b62b03f57be
PierreMonvoisin <monvoisin.pierre@gmail.com>
2019-12-06 16:13:23 +0100

    changement de nom

--- Exercice8/index.php
+++ Exercice8/index.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
 <head>
-  <title>Exercice 7</title>
+  <title>Exercice 8</title>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
